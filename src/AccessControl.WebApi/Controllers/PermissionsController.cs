b8206f29889b2751df2b54ae85cd2f875a31e45c
Joao-P Medeiros <Joao-P.Medeiros@btgpactual.com>
2022-03-14 16:30:48 -0300

    chore: start project scaffolding

---
+++ src/AccessControl.WebApi/Controllers/PermissionsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AccessControl.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PermissionsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetPermissionsAsync()
+        {
+            await Task.Delay(1000);
+            return new List<string> { "content" };
+        }
+    }
+}
