9059f292adcb80d278ec42eb6abbfaa97946200e
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-03-15 09:48:12 -0300

    feat: dynamodb record to entities

--- src/AccessControl.WebApi/Models/Access.cs
+++ src/AccessControl.WebApi/Models/Access.cs
@@ -1,4 +1,6 @@
 ﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace AccessControl.WebApi.Models
@@ -8,7 +10,21 @@ namespace AccessControl.WebApi.Models
     {
         public string Group { get; set; }
         public string Domain { get; set; }
+        [DynamoDBProperty(typeof(RuleConverter))]
         public IEnumerable<Rule> Rules { get; set; }
+    }
+
+    public class RuleConverter : IPropertyConverter
+    {
+        public object FromEntry(DynamoDBEntry entry)
+        {
+            return JsonConvert.DeserializeObject<IEnumerable<Rule>>(entry);
+        }
+
+        public DynamoDBEntry ToEntry(object value)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
     public class Rule
